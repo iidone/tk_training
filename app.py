6f7dfc52270bae794bcd86ba56a48df8ca5fc173
iidone <iidone@mail.ru>
2026-03-18 21:27:05 +0300

    final

--- app.py
+++ app.py
@@ -7,6 +7,18 @@ from styles import (
 )
 from PIL import Image, ImageTk
 import os
+import sys
+
+
+def get_resource_path(relative_path):
+    """Get absolute path to resource, works for dev and for PyInstaller."""
+    try:
+        # PyInstaller creates a temp folder and stores path in _MEIPASS
+        base_path = sys._MEIPASS
+    except Exception:
+        base_path = os.path.abspath(".")
+    
+    return os.path.join(base_path, relative_path)
 
 
 class LoginWindow(tk.Tk):
